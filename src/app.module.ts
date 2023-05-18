ba69edf32a4b4a05dbebb88ca9f3a4938da6abef
Uzeyr OZCAN <uzeyr@oktein.com>
2023-05-18 11:39:34 +0300

    migration sript

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,22 +10,16 @@ import { ProductModule } from './product/product.module';
 import { Product } from './product/entities/product.entity';
 import { CategoryModule } from './category/category.module';
 import { Category } from './category/entities/category.entity';
+import { getConfig } from '../db/ds.config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mysql',
-        host: process.env.MYSQL_HOST,
-        port: parseInt(process.env.MYSQL_HOST),
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        entities: [User, Product, Category],
-        synchronize: true,
-      }),
+      useFactory: () => (
+        getConfig()
+      ),
     }),
     /*
     TypeOrmModule.forRoot({
